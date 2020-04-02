018720c54c2807d67dd96f596927a36a25028077
Johnny <glaubergedoz@gmail.com>
2020-04-02 17:18:17 -0300

    Comentado código do voyager com erro

--- app/Http/Controllers/Voyager/VoyagerBaseController.php
+++ app/Http/Controllers/Voyager/VoyagerBaseController.php
@@ -2,9 +2,96 @@
 
 namespace App\Http\Controllers\Voyager;
 
+use Illuminate\Http\Request;
+use TCG\Voyager\Facades\Voyager;
 use TCG\Voyager\Http\Controllers\VoyagerBaseController as BaseVoyagerBaseController;
 
 class VoyagerBaseController extends BaseVoyagerBaseController
 {
-    //
+    /**
+     * Get BREAD relations data.
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public function relation(Request $request)
+    {
+        $slug = $this->getSlug($request);
+        $page = $request->input('page');
+        $on_page = 50;
+        $search = $request->input('search', false);
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        $method = $request->input('method', 'add');
+
+        $model = app($dataType->model_name);
+        if ($method != 'add') {
+            $model = $model->find($request->input('id'));
+        }
+
+        $this->authorize($method, $model);
+
+        $rows = $dataType->{$method.'Rows'};
+        foreach ($rows as $key => $row) {
+            if ($row->field === $request->input('type')) {
+                $options = $row->details;
+                $model = app($options->model);
+                $skip = $on_page * ($page - 1);
+
+                // Apply local scope if it is defined in the relationship-options
+                //TODO: corrigir bug no código comentado abaixo
+                /*if ($options->scope && $options->scope != '' && method_exists($model, 'scope'.ucfirst($options->scope))) {
+                    $model = $model->{$options->scope}();
+                }*/
+
+                // If search query, use LIKE to filter results depending on field label
+                if ($search) {
+                    // If we are using additional_attribute as label
+                    if (in_array($options->label, $model->additional_attributes ?? [])) {
+                        $relationshipOptions = $model->all();
+                        $relationshipOptions = $relationshipOptions->filter(function ($model) use ($search, $options) {
+                            return stripos($model->{$options->label}, $search) !== false;
+                        });
+                        $total_count = $relationshipOptions->count();
+                        $relationshipOptions = $relationshipOptions->forPage($page, $on_page);
+                    } else {
+                        $total_count = $model->where($options->label, 'LIKE', '%'.$search.'%')->count();
+                        $relationshipOptions = $model->take($on_page)->skip($skip)
+                            ->where($options->label, 'LIKE', '%'.$search.'%')
+                            ->get();
+                    }
+                } else {
+                    $total_count = $model->count();
+                    $relationshipOptions = $model->take($on_page)->skip($skip)->get();
+                }
+
+                $results = [];
+
+                if (!$row->required && !$search) {
+                    $results[] = [
+                        'id'   => '',
+                        'text' => __('voyager::generic.none'),
+                    ];
+                }
+
+                foreach ($relationshipOptions as $relationshipOption) {
+                    $results[] = [
+                        'id'   => $relationshipOption->{$options->key},
+                        'text' => $relationshipOption->{$options->label},
+                    ];
+                }
+
+                return response()->json([
+                    'results'    => $results,
+                    'pagination' => [
+                        'more' => ($total_count > ($skip + $on_page)),
+                    ],
+                ]);
+            }
+        }
+
+        // No result found, return empty array
+        return response()->json([], 404);
+    }
 }

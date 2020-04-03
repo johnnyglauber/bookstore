e10916d5e4b6d6c2597454be0c4ede43da2bbfd5
Johnny <glaubergedoz@gmail.com>
2020-04-02 22:42:26 -0300

    Vendas

---
+++ app/Http/Controllers/SaleController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Voyager\VoyagerBaseController;
+use Illuminate\Http\Request;
+use TCG\Voyager\Events\BreadDataAdded;
+use TCG\Voyager\Facades\Voyager;
+
+class SaleController extends VoyagerBaseController
+{
+    /**
+     * POST BRE(A)D - Store data.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $slug = $this->getSlug($request);
+
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Check permission
+        $this->authorize('add', app($dataType->model_name));
+
+        // Validate fields with ajax
+        $val = $this->validateBread($request->all(), $dataType->addRows)->validate();
+        $request->merge(['user_id' => auth()->user()->id]);
+        $data = $this->insertUpdateData($request, $slug, $dataType->addRows, new $dataType->model_name());
+
+        event(new BreadDataAdded($dataType, $data));
+
+        if (!$request->has('_tagging')) {
+            if (auth()->user()->can('browse', $data)) {
+                $redirect = redirect()->route("voyager.{$dataType->slug}.index");
+            } else {
+                $redirect = redirect()->back();
+            }
+
+            return $redirect->with([
+                'message'    => __('voyager::generic.successfully_added_new')." {$dataType->getTranslatedAttribute('display_name_singular')}",
+                'alert-type' => 'success',
+            ]);
+        } else {
+            return response()->json(['success' => true, 'data' => $data]);
+        }
+    }
+}
---
+++ app/Widgets/CustomerDimmer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Widgets;
+
+use App\Sale;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Widgets\BaseDimmer;
+
+class SaleDimmer extends BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = Sale::count();
+        $string = trans_choice('Venda(s)', $count);
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-dollar',
+            'title'  => "{$count} {$string}",
+            'text'   => __('Tem :count :string no seu banco de dados. Clique no botão abaixo para ver todas as vendas.', ['count' => $count, 'string' => Str::lower($string)]),
+            'button' => [
+                'text' => 'Vendas',
+                'link' => route('voyager.sales.index'),
+            ],
+            'image' => voyager_asset('images/widget-backgrounds/02.jpg'),
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return Auth::user()->can('browse', Voyager::model('User'));
+    }
+}
